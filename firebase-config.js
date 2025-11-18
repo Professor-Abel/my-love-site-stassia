1fc0efb94cffb639cae83805ee6e9cb2986c35f3
Professor-Abel <melqabel@gmail.com>
2025-11-18 10:02:45 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,10 @@
-// firebase-config.js
-
-// Конфиг твоего проекта Firebase
-export const firebaseConfig = {
-  apiKey: "AIzaSyCbg08b96hAGU3kwvkjsv1x1Is-879Mbgc",
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCbgO8b96hAGU3kvwkjsv1x1Is-879Mbgc",
   authDomain: "asyaman-40f1f.firebaseapp.com",
   projectId: "asyaman-40f1f",
   storageBucket: "asyaman-40f1f.firebasestorage.app",
   messagingSenderId: "780594675672",
   appId: "1:780594675672:web:ccd9c524a20721ba81bcad",
-  measurementId: "G-MMMTD9XENH",
-};
-
-// Твой UID, чтобы только ты видел админ-панель
-export const ADMIN_UID = "QgveUKbsJLUOA3oehvXgTEbTg1S2";
+  measurementId: "G-MMMTD9XENH"
+}
